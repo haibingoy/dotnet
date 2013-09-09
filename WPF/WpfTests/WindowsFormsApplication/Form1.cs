02f0983ce0df9641d668f72d8802b598aa8114d1
haibing <haibingoy@hotmail.com>
2013-09-09 14:35:44 +0800

    Add wpf test.

---
+++ WPF/WpfTests/WindowsFormsApplication/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            var winformButton = new Button();
+            winformButton.Width = 200;
+            winformButton.Height = 30;
+            winformButton.Text = "OK";
+            this.Controls.Add(winformButton);
+        }
+    }
+}
---
+++ WPF/WpfTests/WindowsFormsHost/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows = System.Windows.Forms;
+
+namespace WindowsFormsHost
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Windows.Application.EnableVisualStyles();
+            Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var winformButton = new Windows.Button();
+            winformButton.Width = 200;
+            winformButton.Height = 30;
+            winformButton.Text = "OK";
+            winformButton.Click += winformButton_Click;
+            WindowsFormsHost.Child = winformButton;
+        }
+
+        void winformButton_Click(object sender, EventArgs e)
+        {
+            var form = new Form1();
+            form.Show();
+        }
+    }
+}
