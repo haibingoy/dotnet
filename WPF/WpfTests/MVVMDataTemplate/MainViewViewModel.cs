acfc42430ba44999ce7fd43cd0da1086d9fd5f71
haibing <haibingoy@hotmail.com>
2013-11-07 15:58:36 +0800

    max and min width

---
+++ WPF/WpfTests/MVVMDataTemplate/MainViewViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MVVMDataTemplate
+{
+    public class MainViewViewModel
+    {
+        public MainViewViewModel()
+        {
+            Item1 = new Item1ViewModel();
+            Item2 = new Item2ViewModel();
+            Button = new Button();
+            Button.Content = "Test Button";
+        }
+
+        public object Item1 { get; set; }
+
+        public object Item2 { get; set; }
+
+        public Button Button { get; set; }
+    }
+}
