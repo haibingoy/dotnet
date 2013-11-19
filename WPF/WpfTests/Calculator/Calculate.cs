11cea5740e4ef00c3811e53fe15f192d0419b548
haibing <haibingoy@hotmail.com>
2013-11-19 15:10:20 +0800

    How to use pdb file of other assemblies? Debug into it?

---
+++ WPF/WpfTests/Calculator/Calculate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculate
+    {
+        public static int Add(int a, int b)
+        {
+            int result = 0;
+            result += a;
+            result += b;
+            return result;
+        }
+    }
+}
